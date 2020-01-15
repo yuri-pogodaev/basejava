eef1eb9a49b3110d05890df0a4dcc0e3d258b689
Yuri Pogodaev <pogodaev.yuri@gmail.com>
2020-01-15 09:56:38 +0300

    HW 3 implements interface

---
+++ src/com/basejava/webapp/storage/Storage.java
@@ -0,0 +1,22 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public interface Storage {
+
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    Resume[] getAll();
+
+    int size();
+}
