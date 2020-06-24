6a1bfa45e05dbd40abafc2456d402967df89eb34
Yuri Pogodaev <pogodaev.yuri@gmail.com>
2020-06-24 21:11:29 +0300

    lesson16 jsp + include

--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -1,7 +1,6 @@
 package com.basejava.webapp.web;
 
 import com.basejava.webapp.Config;
-import com.basejava.webapp.model.Resume;
 import com.basejava.webapp.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -10,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
 
 
 public class ResumeServlet extends HttpServlet {
@@ -28,37 +25,8 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-//        String name = request.getParameter("name");
-//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + "!");
-        PrintWriter writer = response.getWriter();
-        writer.write(
-                " <html>\n" +
-                        "<head>\n" +
-                        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-                        "<link rel=\"stylesheet\" href=\"css/style.css\">\n " +
-                        "<title>Резюме</title>\n" +
-                        "</head>\n" +
-                            "<body>\n" +
-                        "<section>\n" +
-                        "<table border =\"1\" cellpading=\"8\" cellspacing=\"0\">\n" +
-                        "<tr>\n" +
-                        " <th>UUID</th>\n" +
-                        " <th>Full_Name</th>\n" +
-                        " </tr>\n");
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr>\n" +
-                            "<td>" + resume.getUuid() + "</td>\n" +
-                            "<td>" + resume.getFullName() + "</td>\n" +
-                            "</tr>\n");
-        }
-        writer.write(
-                "</table>" +
-                        "</section>\n" +
-                        "</body>\n" +
-                        "</html>\n");
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+
     }
 }
